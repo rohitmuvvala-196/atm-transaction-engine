2ab8f378a7cb70c09573c74a6b2c5d0e13ec76f8
MUVVALA NAGA SAI ROHIT <198780374+rohitmuvvala-196@users.noreply.github.com>
2026-07-08 20:01:02 +0530

    Add backend/src/main/java/com/atmengine/constants/TransactionType.java

---
+++ backend/src/main/java/com/atmengine/constants/TransactionType.java
@@ -0,0 +1,12 @@
+package com.atmengine.constants;
+
+public enum TransactionType {
+    WITHDRAWAL,
+    DEPOSIT,
+    TRANSFER,
+    PIN_CHANGE,
+    LOGIN,
+    LOGOUT,
+    BALANCE_INQUIRY,
+    MINI_STATEMENT
+}
