1dee92e21f9a846dc25c57e18fae4eb382dd0713
MUVVALA NAGA SAI ROHIT <198780374+rohitmuvvala-196@users.noreply.github.com>
2026-07-08 20:00:57 +0530

    Add backend/src/main/java/com/atmengine/AtmEngineApplication.java

---
+++ backend/src/main/java/com/atmengine/AtmEngineApplication.java
@@ -0,0 +1,11 @@
+package com.atmengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AtmEngineApplication.class, args);
+    }
+}
